4627e99a9502fc837aecb181f79f2e775b468666
alaaelgndy <alaaelgndy21@gmail.com>
2018-11-29 22:04:10 +0200

    rename trees package to be binary tree , add helpers functions like hasRight,hasLeft,isLeaf and get node by id using find function

---
+++ binarytrees/trees.go
@@ -0,0 +1,85 @@
+package binarytrees
+
+import "errors"
+
+// BinaryTree root
+type BinaryTree struct {
+	node *Node
+}
+
+// Node struct
+type Node struct {
+	id    int
+	name  string
+	left  *Node
+	right *Node
+}
+
+func newBinaryTree() *BinaryTree {
+	return &BinaryTree{nil}
+}
+
+func addRoot(node *Node) *BinaryTree {
+	newBinaryTree := &BinaryTree{
+		&Node{
+			node.id,
+			node.name,
+			nil,
+			nil,
+		},
+	}
+	return newBinaryTree
+}
+
+func (n *Node) insert(node *Node) *Node {
+	var newNode *Node
+	if node == nil {
+		newNode = addRoot(node).node
+	}
+	if node.id > n.id {
+		if n.right == nil {
+			n.right = &Node{node.id, node.name, nil, nil}
+			newNode = n.right
+		} else {
+			newNode = n.right.insert(node)
+		}
+	}
+	if node.id < n.id {
+		if n.left == nil {
+			n.left = &Node{node.id, node.name, nil, nil}
+			newNode = n.left
+		} else {
+			newNode = n.left.insert(node)
+		}
+	}
+	return newNode
+}
+
+func (t *BinaryTree) retrive() *BinaryTree {
+	return t
+}
+
+func (n *Node) find(id int) (*Node, error) {
+	if id == n.id {
+		return n, nil
+	}
+	if id > n.id && n.hasRight() {
+		return n.right.find(id)
+	}
+	if id < n.id && n.hasLeft() {
+		return n.left.find(id)
+	}
+	return n, errors.New("this id not found")
+}
+
+func (n *Node) isLeaf() bool {
+	return !n.hasLeft() && !n.hasRight()
+}
+
+func (n *Node) hasLeft() bool {
+	return n.left != nil
+}
+
+func (n *Node) hasRight() bool {
+	return n.right != nil
+}
---
+++ binarytrees/trees_test.go
@@ -0,0 +1,107 @@
+package binarytrees
+
+import (
+	"testing"
+)
+
+func TestAddRoot(t *testing.T) {
+	BinaryTree := addRoot(&Node{0, "alaa1", nil, nil})
+	if BinaryTree.node.id != 0 {
+		t.Error("expected", 0, "got", BinaryTree.node.id)
+	}
+}
+
+var lastBinaryTree *BinaryTree
+var lastDeepInLeft *Node
+var lastDeepInRight *Node
+
+// Test will handle root with deepest element in right and in left only
+func TestInsert(t *testing.T) {
+	BinaryTree := addRoot(&Node{5, "node", nil, nil}) // root
+	node := BinaryTree.node
+	node.insert(&Node{9, "node1", nil, nil})                // first right
+	node.insert(&Node{12, "node2", nil, nil})               // right for node1
+	node.insert(&Node{3, "node3", nil, nil})                // left for node
+	node.insert(&Node{7, "node4", nil, nil})                // left for node1
+	deepInRight := node.insert(&Node{8, "node5", nil, nil}) // right for node4
+	node.insert(&Node{3, "node6", nil, nil})                // left for root
+	deepInLeft := node.insert(&Node{4, "node7", nil, nil})  // right for node6
+
+	if node.right.left.right.name != deepInRight.name {
+		t.Error("expected", deepInRight.name, "got", node.right.left.right.name)
+	}
+
+	if node.left.right.name != deepInLeft.name {
+		t.Error("expected", deepInLeft.name, "got", node.left.right.name)
+	}
+	lastBinaryTree = BinaryTree
+	lastDeepInLeft = deepInLeft
+	lastDeepInRight = deepInRight
+}
+
+func TestRetrive(t *testing.T) {
+	BinaryTree := lastBinaryTree.retrive()
+	if BinaryTree.node.right.left.right.name != lastDeepInRight.name {
+		t.Error("expected", lastDeepInRight.name, "got", BinaryTree.node.right.left.right.name)
+	}
+
+	if BinaryTree.node.left.right.name != lastDeepInLeft.name {
+		t.Error("expected", lastDeepInLeft.name, "got", BinaryTree.node.left.right.name)
+	}
+}
+
+func TestFind(t *testing.T) {
+	// root node
+	if node, _ := lastBinaryTree.node.find(5); node.name != "node" {
+		t.Error("expected", "node", "got", node.name)
+	}
+	// test not found id
+	if _, err := lastBinaryTree.node.find(50); err == nil {
+		t.Errorf("expect error here")
+	}
+	// test deep element in right
+	if node, _ := lastBinaryTree.node.find(lastDeepInRight.id); node.name != lastDeepInRight.name {
+		t.Error("expected", lastDeepInRight.name, "got", node.name)
+	}
+	// test deep element in left
+	if node, _ := lastBinaryTree.node.find(lastDeepInLeft.id); node.name != lastDeepInLeft.name {
+		t.Error("expected", lastDeepInLeft.name, "got", node.name)
+	}
+
+}
+
+func TestIsLeaf(t *testing.T) {
+	testLeafTree := addRoot(&Node{5, "node", nil, nil}) // root
+	testLeafNode := testLeafTree.node
+	if testLeafNode.isLeaf() != true {
+		t.Error("expected", true, "got", false)
+	}
+	testLeafNode.insert(&Node{9, "node1", nil, nil}) // first right
+	if testLeafNode.isLeaf() != false {
+		t.Error("expected", false, "got", true)
+	}
+}
+
+func TestHasLeft(t *testing.T) {
+	testHasLeftTree := addRoot(&Node{5, "node", nil, nil}) // root
+	testhasLeftNode := testHasLeftTree.node
+	if testhasLeftNode.hasLeft() != false {
+		t.Error("expected", false, "got", true)
+	}
+	testhasLeftNode.insert(&Node{3, "node6", nil, nil}) // left for root
+	if testhasLeftNode.hasLeft() != true {
+		t.Error("expected", true, "got", false)
+	}
+}
+
+func TestHasRight(t *testing.T) {
+	testHasRightTree := addRoot(&Node{5, "node", nil, nil}) // root
+	testhasRightNode := testHasRightTree.node
+	if testhasRightNode.hasRight() != false {
+		t.Error("expected", false, "got", true)
+	}
+	testhasRightNode.insert(&Node{9, "node1", nil, nil}) // first right
+	if testhasRightNode.hasRight() != true {
+		t.Error("expected", true, "got", false)
+	}
+}
