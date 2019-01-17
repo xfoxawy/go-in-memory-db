6128f09f863d3a4d06809585ccc94937ea713278
alaaelgndy <alaaelgndy21@gmail.com>
2019-01-17 14:07:29 +0200

    add helpers in other file instead of be in logging file

---
+++ logging/logging_helpers.go
@@ -0,0 +1,43 @@
+package logging
+
+import (
+	"log"
+	"os"
+)
+
+func writeInFile(filePath string, content string) bool {
+
+	mutex.Lock()
+	f, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println(content)
+	mutex.Unlock()
+	return true
+}
+
+func createFile(filePath string) string {
+	var _, err = os.Stat(filePath)
+
+	// create file if not exists
+	if os.IsNotExist(err) {
+		var file, _ = os.Create(filePath)
+		defer file.Close()
+	}
+	return filePath
+}
+
+// in_array func
+// helper function in valiadtion
+func in_array(value string, array []string) bool {
+	for _, v := range array {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
